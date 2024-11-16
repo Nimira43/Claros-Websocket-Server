45201f7b249ed7cce4f95b82940d0760dc446239
Nimira43 <n.rabone72@aol.com>
2024-11-16 18:30:01 +0000

    Fixed issue - getElementById on line 5 should be getElementsById.

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let openWsBtn = document.getElementById('open-ws')
 let closeWsBtn = document.getElementById('close-ws')
 let form = document.getElementById('form')
 let socketStatus = document.getElementById('status')
-let table = document.getElementByTagName('ul')[0]
+let table = document.getElementsByTagName('ul')[0]
 let message = document.getElementById('message')
 
 openWsBtn.addEventListener('click', () => {
   openWsBtn.disabled = true
-  openWsBtn.style.background = 'gray'
+  openWsBtn.style.background = 'yellow'
   openWsBtn.style.pointerEvents = 'none'
   openWsBtn.textContent = 'Button Disabled'
   
