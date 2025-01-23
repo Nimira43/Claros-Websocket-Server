b1d13f425e6ce320728196c36f83c271fab3a037
Nimira43 <n.rabone72@aol.com>
2025-01-23 08:29:43 +0000

    Restructure file structure/. Created HTTP server.

---
+++ server/app.js
@@ -0,0 +1,5 @@
+const HTTP = require('http')
+const CONSTANTS = require('./customLibrary/websocketConstants')
+const FUNCTIONS = require('./customLibrary/websocketMethods')
+
+const HTTP_SERVER = HTTP.createServer()
