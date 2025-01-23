c21fb86451c45a95c9110e03248e4da60840ff9f
Nimira43 <n.rabone72@aol.com>
2025-01-23 08:41:19 +0000

    Sending back status code 200 okay response

--- server/app.js
+++ server/app.js
@@ -2,4 +2,8 @@ const HTTP = require('http')
 const CONSTANTS = require('./customLibrary/websocketConstants')
 const FUNCTIONS = require('./customLibrary/websocketMethods')
 
-const HTTP_SERVER = HTTP.createServer()
+const HTTP_SERVER = HTTP.createServer((req, res) => {
+  res.writeHead(200)
+  res.end('OK response')
+})
+
