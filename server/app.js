b8938eed367a1afbff8070bdbcf489cede32a438
Nimira43 <n.rabone72@aol.com>
2025-02-02 20:53:43 +0000

    Iterating over the CUSTOM_ERRORS array and setting up a listener for each error event, ensuring that the server can handle various unexpected issues gracefully. This will help identify and log the errors while safely shutting down the process when necessary.

--- server/app.js
+++ server/app.js
@@ -10,3 +10,10 @@ const HTTP_SERVER = HTTP.createServer((req, res) => {
 HTTP_SERVER.listen(CONSTANTS.PORT, () => {
   console.log(`HTTP Server listening on Port ${CONSTANTS.PORT}`)
 })
+
+CONSTANTS.CUSTOM_ERRORS.forEach( errorEvent => {
+  process.on(errorEvent, (err) => {
+    console.log(`Error Event Alert: ${errorEvent}. Error Object:`, err)
+    process.exit(1)
+  })
+})
