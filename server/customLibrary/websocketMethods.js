f05738e549b4b2f151ed4da7b58433077e09949f
Nimira43 <n.rabone72@aol.com>
2025-03-05 17:15:39 +0000

    Added a method to check if an origin is allowed in websocketMethods.js.

--- server/customLibrary/websocketMethods.js
+++ server/customLibrary/websocketMethods.js
@@ -1,3 +1,9 @@
+const CONSTANTS = require('./websocketConstants')
+
 function isOriginAllowed(origin) {
-  
+  return CONSTANTS.ALLOWED_ORIGINS.includes(origin)
+}
+
+module.exports = {
+  isOriginAllowed,
 }
