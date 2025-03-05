504e0618c7c8a01b610bb6358c0c736c656dec37
Nimira43 <n.rabone72@aol.com>
2025-03-05 17:11:22 +0000

    Added Allowed Origins field into object

--- server/customLibrary/websocketConstants.js
+++ server/customLibrary/websocketConstants.js
@@ -8,6 +8,11 @@ module.exports = {
   METHOD: "GET",
   VERSION: 13,
   CONNECTION: 'upgrade',
-  UPGRADE: 'websocket'
+  UPGRADE: 'websocket',
+  ALLOWED_ORIGINS: [
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+    'null'
+  ],
 }
 
