82adb1b1741e6b3d44f4786fe464642197db7fa0
Nimira43 <n.rabone72@aol.com>
2025-05-20 05:07:22 +0100

    Saved current version as Version-3.

---
+++ 2-Previous-Versions/Version-3/server/app.js
@@ -0,0 +1,42 @@
+const HTTP = require('http')
+const CONSTANTS = require('./customLibrary/websocketConstants')
+const FUNCTIONS = require('./customLibrary/websocketMethods')
+
+const HTTP_SERVER = HTTP.createServer((req, res) => {
+  res.writeHead(200)
+  res.end('OK response')
+})
+
+HTTP_SERVER.listen(CONSTANTS.PORT, () => {
+  console.log(`HTTP Server listening on Port ${CONSTANTS.PORT}`)
+})
+
+CONSTANTS.CUSTOM_ERRORS.forEach( errorEvent => {
+  process.on(errorEvent, (err) => {
+    console.log(`Error Event Alert: ${errorEvent}. Error Object:`, err)
+    process.exit(1)
+  })
+})
+
+HTTP_SERVER.on('upgrade', (req, socket, head) => {
+  const upgradeHeaderCheck = req.headers['upgrade'].toLowerCase() === CONSTANTS.UPGRADE
+  const connectionHeaderCheck = req.headers['connection'].toLowerCase() === CONSTANTS.CONNECTION
+  const methodCheck = req.method === CONSTANTS.METHOD
+  const origin =req.headers['origin']
+  const originCheck = FUNCTIONS.isOriginAllowed(origin)
+
+  if (FUNCTIONS.check(socket, upgradeHeaderCheck, connectionHeaderCheck, methodCheck, originCheck)) {
+    upgradeConnection(req, socket, head)
+  }
+})
+
+function upgradeConnection(req, socket, head) {
+  const clientKey = req.headers['sec-websocket-key']
+  const headers = FUNCTIONS.createUpgradeHeaders(clientKey)
+  socket.write(headers)
+  startWebSocketConnection(socket)
+}
+
+function startWebSocketConnection(socket) {
+
+}
---
+++ 2-Previous-Versions/Version-3/server/customLibrary/websocketMethods.js
@@ -0,0 +1,49 @@
+const CONSTANTS = require('./websocketConstants')
+const crypto = require('crypto')
+
+function isOriginAllowed(origin) {
+  return CONSTANTS.ALLOWED_ORIGINS.includes(origin)
+}
+
+function check(socket, upgradeHeaderCheck, connectionHeaderCheck, methodCheck, originCheck) {
+  if (upgradeHeaderCheck && connectionHeaderCheck && methodCheck && originCheck) {
+    return true
+  } else {
+    const message = '400 bad request. The HTTP headers do not comply with RFC6455 specifactions.'
+    const messageLength = message.length
+    const response = 
+      `HTTP/1.1 400 Bad Request\r\n` +
+      `Content-Type: text/plain\r\n` +
+      `Content-Length: ${messageLength}\r\n` +
+      `\r\n` +
+      message
+    socket.write(response)
+    socket.end()
+  }
+}
+
+function createUpgradeHeaders(clientKey) {
+  let serverKey = generateServerKey(clientKey)
+  let headers = [
+    'HTTP/1.1 101 Switching Protocols',
+    'Upgrade: websocket',
+    'Connection: Upgrade',
+    `Sec-Websocket-Accept: ${serverKey}`
+  ]
+  const upgradeHeaders = headers.join('\r\n') + '\r\n\r\n'
+  return upgradeHeaders
+}
+
+function generateServerKey(clientKey) {
+  let data = clientKey + CONSTANTS.GUID
+  const hash = crypto.createHash('sha1')
+  hash.update(data)
+  let serverKey = hash.digest('base64')
+  return serverKey
+}
+
+module.exports = {
+  isOriginAllowed,
+  check,
+  createUpgradeHeaders,
+}
