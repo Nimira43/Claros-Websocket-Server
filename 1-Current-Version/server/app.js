69c027483797ffa12a3a7dc070d54ec750037e4f
Nimira43 <n.rabone72@aol.com>
2025-07-12 22:40:45 +0100

    Updating startWebSocketConnection function

--- 1-Current-Version/server/app.js
+++ 1-Current-Version/server/app.js
@@ -39,7 +39,9 @@ function upgradeConnection(req, socket, head) {
 
 function startWebSocketConnection(socket) {
   console.log(`Websocket connection established with Client on Port ${socket.remotePort}`)
+  
   socket.on('data', (chunk) => {
-    
+    console.log('Chunk received.')
+    receiver.processBuffer(chunk, socket)
   })
 }
