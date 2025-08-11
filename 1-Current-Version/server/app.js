8ff19da3e2a2b918064147a17f1983e1e7d32422
Nimira43 <n.rabone72@aol.com>
2025-08-11 01:52:23 +0100

    Using the _unmaskPAyload function to create the full_unmasked_payload_buffer

--- 1-Current-Version/server/app.js
+++ 1-Current-Version/server/app.js
@@ -176,7 +176,7 @@ class WebSocketReceiver {
 
     let full_masked_payload_buffer = this.consumePayload(this._framePayloadLength)
 
-    let full_unmasked_payload_buffer 
+    let full_unmasked_payload_buffer = FUNCTIONS._unmaskPayload(full_masked_payload_buffer, this._mask)
   }
 
   consumePayload(n) {
--- 1-Current-Version/server/customLibrary/websocketMethods.js
+++ 1-Current-Version/server/customLibrary/websocketMethods.js
@@ -53,4 +53,5 @@ module.exports = {
   isOriginAllowed,
   check,
   createUpgradeHeaders,
+  _unmaskPayload
 }
